43a618435165bd1e624a6218190e06fa0b0f2986
Hoa28686 <195274420+Hoa28686@users.noreply.github.com>
2025-04-08 16:04:55 +0300

    finnish jsx part of step2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div >
       <Header className="container"/>
-      <main className="flex container">
+      <main className="flex">
         <PersonList/>
         
       </main>
